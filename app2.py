61db3d56f65fe3ca714f7656bf0f6515cbb0466e
taesaksitt <138297693+taesaksit@users.noreply.github.com>
2024-08-16 21:29:05 +0700

    Add files via upload

---
+++ app2.py
@@ -0,0 +1,72 @@
+import cv2
+from ultralytics import YOLO
+import pyttsx3
+
+# Initialize TTS engine
+engine = pyttsx3.init()
+# Function for speaking
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+# Start webcam
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+
+# Model
+model = YOLO("yolo-Weights/yolov8n.pt")
+
+# Object classes
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
+
+while True:
+    success, img = cap.read()
+    results = model(img, stream=True)
+
+    # Coordinates
+    for r in results:
+        boxes = r.boxes
+
+        for box in boxes:
+            # Bounding box
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)  # convert to int values
+
+            # Put box in cam
+            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+
+            # Class name
+            cls = int(box.cls[0])
+            class_name = classNames[cls]
+            print("Class name -->", class_name)
+
+            # Speak the detected object
+            engine.say(class_name)
+            engine.runAndWait()
+
+            # Object details
+            org = [x1, y1]
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            fontScale = 1
+            color = (255, 0, 0)
+            thickness = 2
+
+            cv2.putText(img, class_name, org, font, fontScale, color, thickness)
+
+    cv2.imshow('Webcam', img)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
